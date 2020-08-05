2564ff9d73eff0765adff53ae48d673af0633154
11hengstenberg <11hengstenberg@gmail.com>
2020-08-05 13:49:04 -0600

    ruido

--- emisor.py
+++ emisor.py
@@ -4,10 +4,11 @@ librerias
 from bitarray import bitarray
 import socket
 import pickle
+from random import randint as rm
 
-"""
-cadena a enviar
-"""
+ruido_bit = 100
+
+
 def cadena():
     data=input("Ingrese la palabra. \n")
     cadena_segura(data)
@@ -18,6 +19,7 @@ def cadena_segura(data):
     cadena_binaria = ''.join(format(ord(x), 'b') for x in data)
     #print ("cadena binaria:", cadena_binaria,'\n')
     cadena_bitarray = bitarray(cadena_binaria,endian='big')
+    cadena_binaria = ruido(cadena_bitarray)
     print (cadena_bitarray)
     pickle_cadena = pickle.dumps(cadena_bitarray)
     cadena_enviada(pickle_cadena)
@@ -31,9 +33,15 @@ def cadena_enviada(cadena_bitarray):
         #print ('cadena normal', cadena_bitarray)
         s.sendall(cadena_bitarray)
         data = s.recv(1024)
-
     #print('Received', data)
 
+def ruido (cadena_binaria):
+    ruido = round(len(cadena_binaria)/ruido_bit)
+    for i in range (ruido if ruido else 1):
+        posicion=rm(0, len (cadena_binaria) -1)
+        print('cadema sin ruido:', cadena_binaria)
+        cadena_binaria[posicion]= not cadena_binaria[posicion]
+        print ('cadena con ruido:', cadena_binaria)
     
 
 
--- receptor.py
+++ receptor.py
@@ -3,8 +3,7 @@ import pickle
 
 def decodificar(pickle_cadena):
     cadena_bits = ''.join([ '1' if x else '0' for x in picke_cadena ])
-    cadena_str = )
-    print (cadena_str)
+    print (cadena_bits)
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
